976ad7b890a1813fcfb4f7fe74207ed61c367d20
Ricardo Arellano <rarellanob@gmail.com>
2021-01-02 01:04:54 -0600

    Express bootstrap

---
+++ packages/service/routes/transactions.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/debit', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/credit', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
